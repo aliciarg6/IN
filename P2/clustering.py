03cb9d95c8141b6266ff5cb3bd1a46686bd399f9
Alberto Armijo Ruiz <armijoalb@correo.ugr.es>
2017-12-12 09:43:57 +0100

    añadidas gráficas

--- P2/clustering.py
+++ P2/clustering.py
@@ -6,20 +6,23 @@ Algoritmos utilizados en el estudio.
 - KMeans.
 - AgglomerativeClustering.
 - MeanShift.
--
+- Birch.
+- SpectralClustering
 """
 
 import matplotlib.pyplot as plt
 import pandas as pd
-
-from sklearn.cluster import KMeans
-from sklearn import metrics
-from sklearn import preprocessing
-import sklearn
+import sys
+from sklearn.cluster import KMeans, AgglomerativeClustering,estimate_bandwidth
+from sklearn.cluster import Birch,SpectralClustering,MeanShift
+from sklearn import metrics, preprocessing
 from math import floor
 import seaborn as sns
 import time
-from sklearn.neighbors import kneighbors_graph
+from scipy.cluster.hierarchy import dendrogram,linkage
+import numpy as np
+
+sys.setrecursionlimit(1500)
 
 t_global_start = time.time()
 
@@ -40,7 +43,7 @@ def makeScatterPlot(data,outputName=None,displayOutput=True):
 
 def calculateMetrics(clusterPredict,data):
     metric_CH = metrics.calinski_harabaz_score(data, clusterPredict)
-    metric_SC = metrics.silhouette_score(data, clusterPredict, metric='euclidean', sample_size=floor(0.1 * len(X)),
+    metric_SC = metrics.silhouette_score(data, clusterPredict, metric='euclidean', sample_size=floor(0.1 * len(data)),
                                         random_state=123456)
 
     return [metric_CH,metric_SC]
@@ -60,24 +63,77 @@ def createPrediction(dataframe,data,model):
 
     return [calculateMetrics(cluster_predict,data),X_dataFrame,time_finish,cluster_predict]
 
+def makeDendogram(data,predict,outputName=None, displayOutput=True):
+    vars = list(data)
+    vars.remove("cluster")
+    dataFrame = data[vars]
+    Z = linkage(dataFrame, 'ward')
+    plt.title("Dendograma")
+    dendrogram(Z, leaf_rotation=90., leaf_font_size=8., truncate_mode='lastp', p=12, show_contracted=True, labels=predict)
 
+    if outputName != None:
+        outputName += "_dendogram.png"
+        plt.savefig(outputName)
+    if displayOutput:
+        plt.show()
+
+def makeHeatMap(data,clusters,outputName=None, displayOutput=True):
+    sns.set()
+    vars = list(data)
+    vars.remove('cluster')
+    table = data[vars].T
+    sum_row = {row:np.sum(table[row]) for row in table}
+    sum_df = pd.DataFrame(sum_row, index=["Suma"])
+    sum_df = sum_df.T
+    table = createDataFrame(dataframe=sum_df,prediction=clusters)
+    table = pd.pivot_table(table,index=table.index,columns="cluster",values="Suma",fill_value=0)
+    print(table)
+    heatmap = sns.heatmap(table.T,xticklabels="auto", yticklabels="auto")
+    plt.yticks(rotation=0)
+
+    # Guardamos el archivo con el nombre especificado + terminación en heatmap.
+    if outputName != None:
+        outputName += "_heatmap.png"
+        heatmap.savefig(outputName)
+    if displayOutput:
+        plt.show()
+
+def makeClusterMap(data,clusters,outputName=None,displayOutput=True):
+    sns.set()
+    vars = list(data)
+    vars.remove('cluster')
+    table = data[vars].T
+    sum_row = {row: np.sum(table[row]) for row in table}
+    sum_df = pd.DataFrame(sum_row, index=["Suma"])
+    sum_df = sum_df.T
+    table = createDataFrame(dataframe=sum_df, prediction=clusters)
+    table = pd.pivot_table(table, index=table.index, columns="cluster", values="Suma", fill_value=0)
+    print(table)
+    clustermap = sns.clustermap(table, xticklabels="auto", yticklabels="auto")
+    plt.yticks(rotation=0)
+
+    if(outputName != None):
+        outputName += "clustermap.png"
+        clustermap.savefig(outputName)
+
+    if displayOutput:
+        plt.show()
 
 accidentes = pd.read_csv('accidentes_2013.csv')
 subset = accidentes[accidentes['TIPO_ACCIDENTE'].str.contains("Colisión de vehículos")]
 usadas = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
 X = subset[usadas]
-n = 5000
+n = 500
 
 X  = X.sample(n,random_state=123456)
-
 X_normal = preprocessing.normalize(X, norm='l2')
 
-k_means = KMeans(init='k-means++', n_clusters=4, n_init=5,n_jobs=8)
-aglo=sklearn.cluster.AgglomerativeClustering(n_clusters=40,linkage="ward")
-bandwidth = sklearn.cluster.estimate_bandwidth(X_normal, quantile=0.2, random_state=123456, n_jobs=8)
-meanshift = sklearn.cluster.MeanShift(n_jobs=8,bandwidth=bandwidth,bin_seeding=True)
-birch=sklearn.cluster.Birch(n_clusters=6,threshold=0.1)
-spectral=sklearn.cluster.SpectralClustering(n_clusters=4,eigen_solver="arpack",n_jobs=8)
+k_means = KMeans(init='k-means++', n_clusters=4, n_init=5)
+aglo=AgglomerativeClustering(n_clusters=40,linkage="ward")
+bandwidth = estimate_bandwidth(X_normal, quantile=0.2, random_state=123456)
+meanshift = MeanShift(bandwidth=bandwidth,bin_seeding=True)
+birch=Birch(n_clusters=6,threshold=0.1)
+spectral=SpectralClustering(n_clusters=4,eigen_solver="arpack")
 
 
 clustering_algorithms = (
@@ -88,27 +144,35 @@ clustering_algorithms = (
     ("SpectralC",spectral)
 )
 
-outputData = dict()
-for algorithm_name,algorithm in clustering_algorithms:
-    results = dict()
-    met, clusterFrame, timeAlg,cluster_predict = createPrediction(dataframe=X, data=X_normal, model=algorithm)
-    n_clusters=len(set(cluster_predict))
-    #makeScatterPlot(data=clusterFrame, outputName=algorithm_name, displayOutput=False)
+met, clusterFrame, timeAlg, cluster_predict = createPrediction(dataframe=X, data=X_normal, model=k_means)
+# makeScatterPlot(data=clusterFrame,displayOutput=True)
+# makeDendogram(data=clusterFrame,predict=cluster_predict, outputName="agglomerativeclustering")
+makeHeatMap(data=clusterFrame, clusters=cluster_predict)
+makeClusterMap(data=clusterFrame, clusters=cluster_predict)
 
-    results['n_clusters']=n_clusters
-    results['sc_metric']=met[0]
-    results['hc_metric']=met[1]
-    results['time']=timeAlg
+# Creamos los datos de salida, y mostramos las gráficas si queremos.
 
-    outputData[algorithm_name] = results
-
-print('\n{0:<15}\t{1:<10}\t{2:<10}\t{3:<10}\t{4:<10}'.format(
-    'Name', 'N clusters', 'SC metric', 'HC metric', 'Time (s)'))
-
-for name, res in outputData.items():
-    print('{0:<15}\t{1:<10}\t{2:<10.2f}\t{3:<10.2f}\t{4:<10.2f}'.format(
-        name, res['n_clusters'], res['sc_metric'], res['hc_metric'],
-        res['time']))
-
-print('\nTotal time = {0:.2f}'.format(time.time() - t_global_start))
+# outputData = dict()
+# for algorithm_name,algorithm in clustering_algorithms:
+#     results = dict()
+#     met, clusterFrame, timeAlg,cluster_predict = createPrediction(dataframe=X, data=X_normal, model=algorithm)
+#     n_clusters=len(set(cluster_predict))
+#     #makeScatterPlot(data=clusterFrame, outputName=algorithm_name, displayOutput=True)
+#
+#     results['n_clusters']=n_clusters
+#     results['sc_metric']=met[0]
+#     results['hc_metric']=met[1]
+#     results['time']=timeAlg
+#
+#     outputData[algorithm_name] = results
+#
+# print('\n{0:<15}\t{1:<10}\t{2:<10}\t{3:<10}\t{4:<10}'.format(
+#     'Name', 'N clusters', 'SC metric', 'HC metric', 'Time (s)'))
+#
+# for name, res in outputData.items():
+#     print('{0:<15}\t{1:<10}\t{2:<10.2f}\t{3:<10.2f}\t{4:<10.2f}'.format(
+#         name, res['n_clusters'], res['sc_metric'], res['hc_metric'],
+#         res['time']))
+#
+# print('\nTotal time = {0:.2f}'.format(time.time() - t_global_start))
 
